aacd4aa2ba3fa2a487aced480c221b5353e1e5b8
MicohRo2 <123667453+MicohRo2@users.noreply.github.com>
2023-10-30 14:37:27 -0400

    CSS added and fetch changed
    CSS added and fetch changed

--- ui/src/Profile.js
+++ ui/src/Profile.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import './Profile.css';
 import { useParams } from "react-router-dom";
 import { variables } from "./Variables";
 import { useCookies } from "react-cookie";
+import ReviewCard from './components/ReviewCard.js';
 
 export const Profile = (props) => {
     const { userName } = useParams();
@@ -9,45 +11,47 @@ export const Profile = (props) => {
     const [cookies, setCookie, removeCookie] = useCookies(["user"]);
     const [reviews, setReviews] = useState([]);
 
-    const fetchUser = async () => {
-        const url = variables.API_URL + "Users/name/" + userName;
-        const response = await fetch(url);
-        const json = await response.json();
-        setUser(json[0]);
-        return
-    };
+    const fetchData = async () => {
+        const response = await fetch(variables.API_URL + "Users/name/" + userName);
+        const userJson = await response.json();
+        setUser(userJson[0]);
 
-    const fetchReviews = async () => {
-        const url2 = variables.API_URL + "Reviews/user/" + user.id;
-        const response2 = await fetch(url2);
-        const json2 = await response2.json();
-        console.log(json2);
-        setReviews(json2);
-    };
+        const response2 = await fetch(variables.API_URL + "Reviews/user/" + userJson[0].id);
+        const reviewsJson = await response2.json();
+        console.log(reviewsJson)
+        setReviews(reviewsJson);
+    }
     
     useEffect(() => {
-        fetchUser()
+        fetchData()
     }, []);
-
-    useEffect(() => {
-        fetchReviews()
-    }, [user]);
-
-    
 
     return (
         
         <>
-            <div>
-                {JSON.stringify(cookies["user"]).replaceAll('"', "") === user.username ? <button>Edit</button> : ""}
-                <img src={user.picture}></img>
-                <p>{user.username}</p>
-                <p>{user.email}</p>
-                <p>{user.joined}</p>
-                <p>{user.bio}</p>
+            <div className="profile-container">
+                {JSON.stringify(cookies["user"]).replaceAll('"', "") === user.username ? <button className="edit-button">Edit</button> : ""}
+                <img src={user.picture} className="profile-image" alt="User's Profile Picture"></img>
+                    <p className="username">{user.username}</p>
+                    <p className="email">{user.email}</p>
+                    <p className="joined-date">Joined: {user.joined}</p>
+                    <p className="bio">{user.bio}</p>
             </div>
             <div>
-                <p></p>
+                {reviews.map(rev => (
+                    <ReviewCard
+                        key={rev.id}
+                        id={rev.id}
+                        title={rev.title}
+                        review={rev.review}
+                        gameplay={rev.gameplay}
+                        presentation={rev.presentation}
+                        engagement={rev.engagement}
+                        difficulty={rev.difficulty}
+                        replayable={rev.replayable}
+                        created={rev.created}
+                    />
+                ))}
             </div>
         </>
         
